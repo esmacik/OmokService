a14a0c22cb18a890cc359c4845c77f741a163641
erikmacik <esmacik@miners.utep.edu>
2020-03-03 15:25:58 -0700

    Changing Smart strategy logic

--- src/play/SmartStrategy.php
+++ src/play/SmartStrategy.php
@@ -14,14 +14,24 @@ class SmartStrategy extends Strategy {
         for ($i = 0; $i < count($pointsToTry); $i += 1) {
             $attempt = $pointsToTry[$i];
             $attemptOpposite = $pointsToTry[count($pointsToTry) - 1 - $i];
+            if ($attemptOpposite[0] < 0 || $attemptOpposite[1] < 0 || $attemptOpposite[0] >= SIZE || $attemptOpposite[1] >= SIZE)
+                continue;
+
             if ($game->board[$attempt[0]][$attempt[1]] == 1) {
-                if ($game->board[$attemptOpposite[0]][$attemptOpposite[1]] == 0)
+                if ($game->board[$attemptOpposite[0]][$attemptOpposite[1]] == 0) {
                     return [$attemptOpposite[0], $attemptOpposite[1]];
-                else continue;
+                }
             }
         }
 
-        $random = new RandomStrategy();
-        return $random->suggestMove($game);
+        foreach ($pointsToTry as $attempt) {
+            if ($attempt[0] < 0 || $attempt[1] < 0 || $attempt[0] >= SIZE || $attempt[1] >= SIZE)
+                continue;
+            if ($game->board[$attempt[0]][$attempt[1]] == 0) {
+                return [$attempt[0], $attempt[1]];
+            }
+        }
+
+        return (new RandomStrategy())->$this->suggestMove($this);
     }
 }
